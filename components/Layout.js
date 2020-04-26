838afa9c4e0c6ca5bf176e7dc74145efb7ab085d
brakocevic.miljan@gmail.com <brakocevic.miljan@gmail.com>
2020-04-26 15:45:27 +0200

    add ga to random team generator app

--- components/Layout.js
+++ components/Layout.js
@@ -1,24 +1,36 @@
 import Head from "next/head";
 import Navbar from "./Navbar";
+import { initGA, logPageView } from "./utils/googleAnalytics";
 
-const Layout = (props) => (
-    <div>
-        <Head>
-            <title>Random Team Generator App | {props.title}</title>
-            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-            <link
-                rel="stylesheet"
-                href="https://bootswatch.com/4/cerulean/bootstrap.min.css"/>
-            <link
-                rel="stylesheet"
-                href="https://bootswatch.com/4/cerulean/bootstrap.min.css"/>
-            <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet" />
+class Layout extends React.Component {
+    componentDidMount () {
+        if (!window.GA_INITIALIZED) {
+            initGA()
+            window.GA_INITIALIZED = true
+        }
+        logPageView()
+    }
+    render() {
+        return (
+            <div>
+                <Head>
+                    <title>Random Team Generator App | {this.props.title}</title>
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+                    <link
+                        rel="stylesheet"
+                        href="https://bootswatch.com/4/cerulean/bootstrap.min.css"/>
+                    <link
+                        rel="stylesheet"
+                        href="https://bootswatch.com/4/cerulean/bootstrap.min.css"/>
+                    <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet" />
 
-        </Head>
-        <Navbar/>
-        <div className="container">
-            {props.children}
-        </div>
-    </div>
-);
+                </Head>
+                <Navbar/>
+                <div className="container">
+                    {this.props.children}
+                </div>
+            </div>
+        )
+    }
+}
 export default Layout;
